cdd384a3f4f9c7900b5eea998c6b6ade395061d1
SofiRoadToDev <sofi.workaffairs@gmail.com>
2025-08-29 18:47:31 -0300

    Hay que pasar desde blade a jsx la GUI, los forms

--- resources/js/Pages/Ingreso.jsx
+++ resources/js/Pages/Ingreso.jsx
@@ -4,6 +4,9 @@ import IngresoList from "@/Components/Ingreso/IngresoList";
 function Ingreso() {
     return (
         <Layout>
+             <button className="bg-black text-white py-2 px-3 rounded-sm mt-3 w-1/2 mx-auto">
+                    <Link href="ingresos/create">Nuevo</Link>
+                </button>
             <IngresoList />
         </Layout>
     );
