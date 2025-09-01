1f7fc2ed0c33d34c74ed2178ba27284ab6045036
SofiRoadToDev <sofi.workaffairs@gmail.com>
2025-09-01 15:26:19 -0300

    merging

--- resources/js/Pages/Ingreso/Ingreso.jsx
+++ resources/js/Pages/Ingreso/Ingreso.jsx
@@ -1,23 +1,12 @@
 import Layout from "@/Layouts/Layout";
 import IngresoList from "@/Components/Ingreso/IngresoList";
 import { Link } from "@inertiajs/react";
-<<<<<<< HEAD:resources/js/Pages/Ingreso/Ingreso.jsx
-function Ingreso() {
-    return (
-        <Layout>
-            <div className="flex flex-col">
-                <button className="bg-black text-white py-2 px-3 rounded-sm mt-3 w-1/2 mx-auto">
-                    <Link href="ingresos/create">Nuevo</Link>
-                </button>
-            <IngresoList />
-            </div>
-             
-=======
+
 
 function Ingreso({ingresos}) {
     return (
         <Layout>
-<<<<<<< HEAD
+
             <div className="flex flex-col">
                 <button className="bg-black text-white py-2 px-3 rounded-sm mt-3 w-1/2 mx-auto">
                     <Link href="ingresos/create">Nuevo</Link>
@@ -28,13 +17,7 @@ function Ingreso({ingresos}) {
                 }
                 
             </div>
-=======
-             <button className="bg-black text-white py-2 px-3 rounded-sm mt-3 w-1/2 mx-auto">
-                    <Link href="ingresos/create">Nuevo</Link>
-                </button>
-            <IngresoList />
->>>>>>> refs/remotes/origin/main
->>>>>>> 1ba139bfe203a2faec68059d7f328d06fa1533f9:resources/js/Pages/Ingreso.jsx
+
         </Layout>
     );
 }
